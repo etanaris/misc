cd803d1fb1bbfe2f584e9840b9ca0e8e2b9205f2
Faaya Fulas <faayaabate@gmail.com>
2018-07-03 18:43:38 -0400

    submitted project 1

--- 6.036/hw1_regression.py
+++ 6.036/hw1_regression.py
@@ -12,8 +12,9 @@ n, d = X_train.shape
 def part1():
     ## Input : Arguments to the function
     ## Return : wRR, Final list of values to write in the file
-    term1 = lambda_input * np.eye(d) + np.matmul(X_train.T,X_trian)
+    term1 = lambda_input * np.eye(d) + np.matmul(X_train.T,X_train)
     wRR = np.matmul(np.linalg.inv(term1), np.matmul(X_train.T, y_train))
+##    print ('WRR', wRR, 'shape', wRR.shape)
     return wRR
 
 wRR = part1()  # Assuming wRR is returned from the function
@@ -25,27 +26,36 @@ np.savetxt("wRR_" + str(lambda_input) + ".csv", wRR, delimiter="\n") # write out
 def part2():
     ## Input : Arguments to the function
     ## Return : active, Final list of values to write in the file
-    test_set = dict((i,j) for i,j in zip(range(1, X_test.shape[0]),[row for row in X_test]))
+    test_set = dict((i,j) for i,j in zip(range(1, (X_test.shape[0]+1)),[row for row in X_test]))
     term1 = lambda_input * np.eye(d) + (1/sigma2_input) * np.matmul(X_train.T,X_train)
     E = np.linalg.inv(term1) #dxd
     X_new = X_train 
     output = []
+    loops = max(10, len(test_set))
 
-    for i in range(10):
+    for i in range(loops):
+##        if len(test_set) == 0:
+##            break
         sigma_max = 0
         x_max = np.array([])
         idx_max = -sys.maxsize
         for idx, x_o in test_set.items():
-            sigma_o = sigma2_input + np.matmul(x_0, np.matmul(E, x_0))
+            #print ('X_0', x_o)
+            sigma_o = sigma2_input + np.matmul(x_o, np.matmul(E, x_o))
             if sigma_o > sigma_max:
                 sigma_max = sigma_o
-                x_max = x_0
+                x_max = x_o
                 idx_max = idx
-        X_new= np.vstack([X_new, x_max]      
+##        print ('X_new', X_new, 'X_new shape', X_new.shape)
+##        print ('x_max', x_max, 'x_max shape', x_max.shape)
+##        print ('sigma_max', sigma_max)
+##        print ('test_set', len(test_set))
+        X_new= np.vstack([X_new, x_max])      
         term1_new = lambda_input * np.eye(d) + (1/sigma2_input) * np.matmul(X_new.T, X_new)
         E = np.linalg.inv(term1_new)
         output.append(idx_max)
-        del test_set[idx_max]    
+        del test_set[idx_max]
+##        print (output)
     return output        
 
 active = part2()  # Assuming active is returned from the function
